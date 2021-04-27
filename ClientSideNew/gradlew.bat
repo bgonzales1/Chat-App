983f2a92e9a6b4bcf6bcf5c077a2dd8dac7e6f8c
Bryan Gonzales <54722032+bgonzales1@users.noreply.github.com>
2021-04-27 13:32:59 -0400

    Update gradlew.bat

--- ClientSideNew/gradlew.bat
+++ ClientSideNew/gradlew.bat
@@ -1,17 +1,17 @@
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem 
 @rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
+@rem 
+@rem 
+@rem 
 @rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem      
 @rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
+@rem 
+@rem 
+@rem 
+@rem 
+@rem 
 @rem
 
 @if "%DEBUG%" == "" @echo off
